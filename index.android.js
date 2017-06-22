f5b64bf866079b139e44b505a6945d52803a5a07
Natanael <natanaeloktavian@gmail.com>
2017-06-22 19:36:44 +0700

    fix jest error

--- index.android.js
+++ index.android.js
@@ -2,4 +2,4 @@ import {AppRegistry} from'react-native'
 import DrawerPageNavigator from'./src/router/Router'
 //import TestStackNavigator from'./src/router/Router'
 
-AppRegistry.registerComponent('ReactNavigation', () => DrawerPageNavigator);
+export default AppRegistry.registerComponent('ReactNavigation', () => DrawerPageNavigator);
